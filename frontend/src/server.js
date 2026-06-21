aebcbfdda1dc047b71818782f5055c0982a6be2d
YashSaxena070 <saxenayash0011@gmail.com>
2026-06-21 12:23:39 +0530

    edit package.json

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
 export const server =
-    import.meta.env.REACT_APP_API_URL || "http://localhost:8000/api/v2";
+    "https://vendoz-dynamic-ecommerce-platform.onrender.com/api/v2";
 
 export const backend_url =
-    import.meta.env.REACT_APP_BACKEND_URL || "http://localhost:8000/";
+    "https://vendoz-dynamic-ecommerce-platform.onrender.com/";
